97ce6c2ba5c59e6ac847bb671d777c82d784e060
Maksim Cornijs <maksimscornijs@gmail.com>
2024-02-05 20:01:33 +0100

    Weather app added OpenWeather API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,81 @@
+"use client";
+
 import Navbar from "@/components/Navbar";
-import Image from "next/image";
+import axios from "axios";
+import { useQuery } from "react-query";
+
+interface WeatherDetail {
+  dt: number;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    sea_level: number;
+    grnd_level: number;
+    humidity: number;
+    temp_kf: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  visibility: number;
+  pop: number;
+  sys: {
+    pod: string;
+  };
+  dt_txt: string;
+}
+
+interface WeatherData {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: WeatherDetail[];
+  city: {
+    id: number;
+    name: string;
+    coord: {
+      lat: number;
+      lon: number;
+    };
+    country: string;
+    population: number;
+    timezone: number;
+    sunrise: number;
+    sunset: number;
+  };
+}
 
 export default function Home() {
+  const { isLoading, error, data } = useQuery<WeatherData>(
+    "repoData",
+    async () => {
+      const { data } = await axios.get(
+        `http://api.openweathermap.org/data/2.5/forecast?q=riga&appid=${process.env.NEXT_PUBLIC_WEATER_KEY}=56`
+      );
+      return data;
+    }
+    // fetch(
+    //   "http://api.openweathermap.org/data/2.5/forecast?q=riga&appid=68c631402ae5c12514f5089b905d7458&cnt=56"
+    // ).then((res) => res.json())
+  );
+
+  console.log("data", data?.city.country);
+
+  if (isLoading) return "Loading...";
   return (
     <div className="flex flex-col gap-4 bg-gray-100 mi-h-screen">
       <Navbar />
--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,10 +1,83 @@
+import { cn } from "@/utils/cn";
 import React from "react";
 import { IoSearchSharp } from "react-icons/io5";
 
-export default function SearchBar() {
+type Props = {
+  className?: string;
+  value: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement> | undefined;
+  onSubmit: React.FormEventHandler<HTMLFormElement> | undefined;
+};
+
+// http://api.openweathermap.org/data/2.5/forecast?q=riga&appid=68c631402ae5c12514f5089b905d7458&cnt=2
+
+interface WeatherDetail {
+  dt: number;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    sea_level: number;
+    grnd_level: number;
+    humidity: number;
+    temp_kf: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  visibility: number;
+  pop: number;
+  sys: {
+    pod: string;
+  };
+  dt_txt: string;
+}
+
+interface WeatherData {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: WeatherDetail[];
+  city: {
+    id: number;
+    name: string;
+    coord: {
+      lat: number;
+      lon: number;
+    };
+    country: string;
+    population: number;
+    timezone: number;
+    sunrise: number;
+    sunset: number;
+  };
+}
+
+export default function SearchBar(props: Props) {
   return (
-    <form className="flex relative items-center justify-center h-10">
+    <form
+      onSubmit={props.onSubmit}
+      className={cn(
+        "flex relative items-center justify-center h-10",
+        props.className
+      )}
+    >
       <input
+        value={props.value}
+        onChange={props.onChange}
         type="text"
         placeholder="Search"
         className="px-4 py-2 w-[230px] border border-gray-300 rounded-l-md focus:border-blue-500 h-full"
