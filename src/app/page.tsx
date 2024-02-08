99c53ec87e14a050644d3c6e7606a4b77b662730
Maksim Cornijs <maksimscornijs@gmail.com>
2024-02-08 18:53:16 +0100

    Weather App working on displaying temp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import Container from "@/components/Container";
 import Navbar from "@/components/Navbar";
+import { convertTemperature } from "@/utils/convertKalvinToCelsius";
 import axios from "axios";
+import { format, parseISO } from "date-fns";
 import { useQuery } from "react-query";
 
 interface WeatherDetail {
@@ -64,21 +67,59 @@ export default function Home() {
     "repoData",
     async () => {
       const { data } = await axios.get(
-        `http://api.openweathermap.org/data/2.5/forecast?q=riga&appid=${process.env.NEXT_PUBLIC_WEATER_KEY}=56`
+        `https://api.openweathermap.org/data/2.5/forecast?q=riga&appid=${process.env.NEXT_PUBLIC_WEATER_KEY}&cnt=56`
       );
       return data;
     }
-    // fetch(
-    //   "http://api.openweathermap.org/data/2.5/forecast?q=riga&appid=68c631402ae5c12514f5089b905d7458&cnt=56"
-    // ).then((res) => res.json())
   );
 
-  console.log("data", data?.city.country);
+  const firstData = data?.list[0];
 
-  if (isLoading) return "Loading...";
+  console.log("data", data);
+
+  if (isLoading)
+    return (
+      <div className="flex items-center min-h-screen justify-center">
+        <p className="animate-bounce">Loading...</p>
+      </div>
+    );
   return (
     <div className="flex flex-col gap-4 bg-gray-100 mi-h-screen">
       <Navbar />
+      <main className="px-3 max-w-7x1 mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
+        <section className="space-y-4">
+          <div className="space-y-2">
+            <h2 className="flex gap-1 text-2xl items-end">
+              <p>{format(parseISO(firstData?.dt_txt ?? ""), "EEEE")}</p>
+              <p className="text-xl">
+                ({format(parseISO(firstData?.dt_txt ?? ""), "dd.MM.yyyy")})
+              </p>
+            </h2>
+            <Container className="gap-10 px-6 items-center">
+              <div className="flex flex-col px-4">
+                <span className="text-5xl">
+                  {convertTemperature(firstData?.main.temp ?? 0)}°
+                </span>
+                <p className="text-xs space-x-1 whitespace-nowrap">
+                  <span>Feels like</span>
+                  <span>
+                    {convertTemperature(firstData?.main.feels_like ?? 0)}°
+                  </span>
+                </p>
+                <p className="text-xs space-x-2">
+                  <span>
+                    {convertTemperature(firstData?.main.temp_min ?? 0)}°↓
+                  </span>
+                  <span>
+                    {convertTemperature(firstData?.main.temp_max ?? 0)}°↑
+                  </span>
+                </p>
+              </div>
+            </Container>
+          </div>
+        </section>
+        <section></section>
+      </main>
     </div>
   );
 }
---
+++ src/utils/convertKalvinToCelsius.ts
@@ -0,0 +1,4 @@
+export function convertTemperature(tempInKelvin: number): number {
+  const tempInCelcius = tempInKelvin - 273.15;
+  return Math.floor(tempInCelcius);
+}
