2e9c996022a978e49cfcf323948e9e057f9ae5a2
Maksim Cornijs <maksimscornijs@gmail.com>
2024-02-19 19:01:45 +0100

    Weather app added a weekly forekast

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Container from "@/components/Container";
+import ForecastSection from "@/components/ForecastSection";
 import Navbar from "@/components/Navbar";
 import WeatherDetails from "@/components/WeatherDetails";
 import WeatherIcon from "@/components/WeatherIcon";
@@ -80,6 +81,22 @@ export default function Home() {
   const firstData = data?.list[0];
 
   console.log("data", data);
+
+  const uniqueDates = [
+    ...new Set(
+      data?.list.map(
+        (entry) => new Date(entry.dt * 1000).toISOString().split("T")[0]
+      )
+    ),
+  ];
+
+  const firstDataForEachDate = uniqueDates.map((date) => {
+    return data?.list.find((entry) => {
+      const entryDate = new Date(entry.dt * 1000).toISOString().split("T")[0];
+      const entryTime = new Date(entry.dt * 1000).getHours();
+      return entryDate === date && entryTime >= 6;
+    });
+  });
 
   if (isLoading)
     return (
@@ -166,6 +183,31 @@ export default function Home() {
         </section>
         <section className="flex w-full flex-col gap-4">
           <p className="text-2xl">Weekly forecast</p>
+          {firstDataForEachDate.map((d, i) => (
+            <ForecastSection
+              key={i}
+              description={d?.weather[0].description ?? ""}
+              weatherIcon={d?.weather[0].icon ?? "01d"}
+              date={format(parseISO(d?.dt_txt ?? ""), "dd.MM")}
+              day={format(parseISO(d?.dt_txt ?? ""), "EEEE")}
+              feels_like={d?.main.feels_like ?? 0}
+              temp={d?.main.temp ?? 0}
+              temp_max={d?.main.temp_max ?? 0}
+              temp_min={d?.main.temp_min ?? 0}
+              airPressure={`${d?.main.pressure} hPa`}
+              humidity={`${d?.main.humidity}%`}
+              sunrise={format(
+                fromUnixTime(data?.city.sunrise ?? 1702517657),
+                "H:mm"
+              )}
+              sunset={format(
+                fromUnixTime(data?.city.sunset ?? 1702517657),
+                "H:mm"
+              )}
+              visability={`${mToKm(d?.visibility ?? 10000)}`}
+              windSpeed={`${mToKmPerHour(d?.wind.speed ?? 1.64)}`}
+            />
+          ))}
         </section>
       </main>
     </div>
--- src/components/ForecastSection.tsx
+++ src/components/ForecastSection.tsx
@@ -1,6 +1,52 @@
 import React from "react";
 import Container from "./Container";
+import WeatherIcon from "./WeatherIcon";
+import WeatherDetails, { WeatherDetailProps } from "./WeatherDetails";
+import { convertTemperature } from "@/utils/convertKalvinToCelsius";
 
-export default function ForecastSection() {
-  return <Container></Container>;
+export interface ForecastWeatherDetailprops extends WeatherDetailProps {
+  weatherIcon: string;
+  date: string;
+  day: string;
+  temp: number;
+  feels_like: number;
+  temp_max: number;
+  temp_min: number;
+  description: string;
+}
+
+export default function ForecastSection(props: ForecastWeatherDetailprops) {
+  const {
+    weatherIcon = "02d",
+    date = "19.09",
+    day = "Tuesday",
+    temp,
+    feels_like,
+    temp_max,
+    temp_min,
+    description,
+  } = props;
+
+  return (
+    <Container className="gap-4">
+      <section className="flex gap-4 items-center px-4">
+        <div className="flex flex-col gap-1 items-center">
+          <WeatherIcon iconName={weatherIcon} />
+          <p>{date}</p>
+          <p className="text-sm">{day}</p>
+        </div>
+        <div className="flex flex-col px-4">
+          <span className="text-5xl">{convertTemperature(temp ?? 0)}°</span>
+          <p className="text=xs space-x-1 whitespace-nowrap">
+            <span>Feels like</span>
+            <span>{convertTemperature(feels_like ?? 0)}°</span>
+          </p>
+          <p className="capitalize">{description}</p>
+        </div>
+      </section>
+      <section className="overflow-x-auto flex justify-between gap-4 px-4 w-full pr-10">
+        <WeatherDetails {...props} />
+      </section>
+    </Container>
+  );
 }
